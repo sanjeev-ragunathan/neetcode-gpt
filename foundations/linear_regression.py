6e353a64f59ddecaf63eb48ac5b32ce4f25fc2d6
Sanjeev Ragunathan <sr2432@cornell.edu>
2026-05-11 11:17:23 -0400

    Update Linear regression forward pass (Math Foundations 5/6)

--- foundations/linear_regression.py
+++ foundations/linear_regression.py
@@ -6,7 +6,8 @@ class Solution:
     def get_model_prediction(self, X: NDArray[np.float64], weights: NDArray[np.float64]) -> NDArray[np.float64]:
         # X is (n, m), weights is (m,) -> return (n,) predictions
         # Round to 5 decimal places
-        Y_hat = X @ np.transpose(weights)
+        # Y_hat = X @ np.transpose(weights)
+        Y_hat = np.dot(X, weights)
         return np.round(Y_hat, 5)
 
     def get_error(self, model_prediction: NDArray[np.float64], ground_truth: NDArray[np.float64]) -> float:
