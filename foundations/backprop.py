390690ae7c55ef491b506a1b9008a789d3bed0d1
Sanjeev Ragunathan <sr2432@cornell.edu>
2026-05-30 14:12:03 -0400

    Implement Backpropagation (Build a Neural Net 2/5)

---
+++ foundations/backprop.py
@@ -0,0 +1,33 @@
+import numpy as np
+from numpy.typing import NDArray
+from typing import Tuple
+
+
+class Solution:
+
+    def backward(self, x: NDArray[np.float64], w: NDArray[np.float64], b: float, y_true: float) -> Tuple[NDArray[np.float64], float]:
+        # x: 1D input array
+        # w: 1D weight array
+        # b: scalar bias
+        # y_true: true target value
+        #
+        # Forward: z = dot(x, w) + b, y_hat = sigmoid(z)
+        # Loss: L = 0.5 * (y_hat - y_true)^2
+        # Return: (dL_dw rounded to 5 decimals, dL_db rounded to 5 decimals)
+
+        z = (x @ w) + b
+        y_hat = 1 / (1 + np.exp(-z))
+        L = 0.5 * np.pow((y_hat - y_true), 2)
+
+        dL_da = (y_hat - y_true)
+        da_dz = y_hat * (1 - y_hat)
+        dz_dw = x
+        dz_db = 1
+
+        dL_dw = dL_da * da_dz * dz_dw
+        dL_db = dL_da * da_dz * dz_db
+
+        return (np.round(dL_dw, 5), np.round(dL_db, 5))
+
+
+
